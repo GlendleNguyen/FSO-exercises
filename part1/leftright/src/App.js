e7ed1a2e618990d859d1b37fd2e7bad1f4189b1b
Glen Nguyen <glendle.nguyen@gmail.com>
2022-06-22 11:13:37 +0800

    handling arrays

---
+++ part1/leftright/src/App.js
@@ -0,0 +1,35 @@
+/**
+ * THIS IS NOT BEST PRACTICE - JUST AN EXAMPLE
+ * STORING ALL OF THE STATE IN A SINGLE OBJECT IS A BAD CHOICE
+ */
+
+import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+
+}
+
+export default App;
