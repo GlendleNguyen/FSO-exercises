4c1593e47993fcdc4f1a650527998d59f7aa5ad4
Glen Nguyen <glendle.nguyen@gmail.com>
2022-06-24 21:08:52 +0800

    bugfix

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0})
+  const [votes, setVotes] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0})
 
   const handleNextAnecdote = () => {
     let i = Math.floor(Math.random() * 7);
